e687c34c5acae7a587d7f3def49960d4c10aec7c
A_SwitchCase <adamsjacques546@gmail.com>
2024-01-29 08:18:37 -0500

    Feat:Add mouse detection plain

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import MyButton from './Components/MyButton'
+import MapPlain from './Components/MapPlain'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
-    <MyButton></MyButton>
+    <MapPlain/>
   </React.StrictMode>,
 )
